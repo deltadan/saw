96ddf1d9b9e69a7f178ad14f7cb468480f366450
Dan Patrick <dan@clouddeltav.com>
2018-05-03 19:31:46 -0400

    SAW tools
    SAW Tools

---
+++ saw-config.ps1
@@ -0,0 +1,22 @@
+###########################################################################################
+# Stormtrooper Project 
+# MIT License - https://github.com/deltadan/stormtrooper
+# Copyright (c) 2018 Dan Patrick - @deltadan
+#
+#Script Purpose
+# - This script will install Chocolatey and the packages listed
+# - This will become for DEV VNET for the Onion Implementation
+###########################################################################################
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'googlechrome',`
+            'visualstudiocode',`
+            'git'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
