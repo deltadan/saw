781323173366ad3b5a93d7e5ab82c01785c8f7a4
deltadan <dan@clouddeltav.com>
2019-02-09 13:59:19 -0800

    update to troubleshoot ext of code

--- saw-config.ps1
+++ saw-config.ps1
@@ -13,10 +13,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'googlechrome',`
-            'visualstudiocode',`
-            'git'
+$Packages = 'visualstudiocode'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
 {choco install $PackageName -y}
+
+
+
