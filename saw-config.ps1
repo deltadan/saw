bf172956095aef35b01d712158bf1b6543a45a4a
Dan Patrick <dan@clouddeltav.com>
2019-03-09 08:32:58 -0500

    update to add chrome and git

--- saw-config.ps1
+++ saw-config.ps1
@@ -13,7 +13,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'visualstudiocode'
+$Packages = 'googlechrome',`
+            'visualstudiocode',`
+            'git'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
